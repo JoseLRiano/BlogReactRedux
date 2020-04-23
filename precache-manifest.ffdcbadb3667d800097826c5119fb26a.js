f9cf17fdcc9b179ef585c513dd85be26988bad7a
JoseLRiano <joselriano@yahoo.com>
2020-04-22 21:55:55 -0400

    Updates

---
+++ precache-manifest.ffdcbadb3667d800097826c5119fb26a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5318bb9a22d7ee1ca1021100910e4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "922de4f9a0edee9a1462",
+    "url": "/static/js/2.c70b6c49.chunk.js"
+  },
+  {
+    "revision": "a680856a4903381d69a83276068aa227",
+    "url": "/static/js/2.c70b6c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0dd2273f4e6bc0c4572",
+    "url": "/static/js/main.d8fcadf2.chunk.js"
+  },
+  {
+    "revision": "430711db2cc41196af7a",
+    "url": "/static/js/runtime-main.9798f641.js"
+  }
+]);
