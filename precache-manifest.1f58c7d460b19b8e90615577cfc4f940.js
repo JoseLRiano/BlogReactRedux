78676cc9d7547eaec2a8fef01ba7936bfaa9502c
JoseLRiano <joselriano@yahoo.com>
2020-04-22 22:11:11 -0400

    Updates

---
+++ precache-manifest.1f58c7d460b19b8e90615577cfc4f940.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "188c43450c794645f5cbe81c5c793cc7",
+    "url": "/BlogReactRedux/index.html"
+  },
+  {
+    "revision": "52d9a03dcf8d7dc8ca15",
+    "url": "/BlogReactRedux/static/js/2.de26408b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/BlogReactRedux/static/js/2.de26408b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a609d8943b98bb34a6b6",
+    "url": "/BlogReactRedux/static/js/main.c32e0911.chunk.js"
+  },
+  {
+    "revision": "ac755fba2401e6f5111e",
+    "url": "/BlogReactRedux/static/js/runtime-main.ea20577e.js"
+  }
+]);
